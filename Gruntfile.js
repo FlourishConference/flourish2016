671f222aa74611a41bc3c38642a67198ec35ffdf
Chase 李 <clee231@uic.edu>
2016-01-09 22:09:37 -0600

    Adding some graphics. Now using scss. Now requires GruntJS to build website. Adding Bourbon mixins. Adding Contact Stub Page.

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(g) {
+
+	g.initConfig({
+		concat: {
+			options: {
+				separator: '/* -------------- */',
+			},
+			js: {
+				src: ['src/js/*.js'],
+				dest: 'js/main.js',
+			},
+			css: {
+				src: ['src/css/*.css'],
+				dest: 'css/main.css',
+			},
+		}, //concat
+
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed',
+				},
+				files: {
+					'css/main.css': 'src/css/main.scss'
+				},
+			},
+			dev: {
+				options: {
+					style: 'nested',
+				},
+				files: {
+					'css/main.css': 'src/css/main.scss'
+				},
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					port: 80,
+					base: ''
+				}
+			}
+		},
+		watch: {
+			options: {
+				livereload: true,
+			},
+			js: {
+				files: ['src/js/**/*.js'],
+				tasks: [],
+			},
+			css: {
+				files: ['src/css/**/*.css', 'src/css/**/*.scss'],
+				tasks: ['sass:dev'],
+			},
+			html: {
+				files: ['*.html'],
+				tasks: [],
+			}
+		},
+
+	});
+
+
+	g.loadNpmTasks('grunt-contrib-concat');
+	g.loadNpmTasks('grunt-contrib-watch');
+	g.loadNpmTasks('grunt-contrib-connect');
+	g.loadNpmTasks('grunt-contrib-sass');
+
+	g.registerTask('default', ['concat', 'sass:dev', 'connect', 'watch']);
+};
